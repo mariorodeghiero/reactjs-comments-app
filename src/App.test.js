8b302a5c2dc2aa14ed9b44ba2699c07f9c8587e2
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-05-26 21:54:36 -0300

    configuring tests with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {shallow} from 'enzyme';
 import App from './App';
 
-import {shallow} from 'enzyme';
-
 describe("<App/>", () => {
-  it('renders without crashing', () => {
+  it("renders without crashing", () => {
     const wrapper = shallow(<App/>)
+    expect(wrapper.length).toBe(1)
+  })
+  it("should have .container class", () => {
+    const wrapper = shallow(<App/>)
+    expect(wrapper.is('.container')).toBe(true)
+  })
+  it("shows Comments", () => {
+    const wrapper = shallow(<App/>)
+    expect(wrapper.find('Comments').length).toBe(1)
+  })
+  it("shows NewComments", () => {
+    const wrapper = shallow(<App/>)
+    expect(wrapper.find('NewComment').length).toBe(1)
   })
 })
-
